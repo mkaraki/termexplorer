b17ef22259baf8ab0d279f2f688d137a2f0e1920
mkaraki <26180919+mkaraki@users.noreply.github.com>
2022-01-09 00:36:58 +0900

    Add help items

--- termexplorer/DocumentWriter/InternalDocuments/Help.cs
+++ termexplorer/DocumentWriter/InternalDocuments/Help.cs
@@ -14,12 +14,16 @@ namespace termexplorer.DocumentWriter.InternalDocuments
             "Q                : Exit this application",
             "F1               : Open this help",
             "F2               : Rename file",
+            "F3               : Search file",
             "F5               : Update current window's directory",
             "Up/Down Arrow    : Select item from current window",
             "Left/Right Arrow : Select window",
+            "Enter/Return     : Open file",
             "Alt + D          : Change current window's directory",
             "Del              : Delete selected item from current window",
-
+            "Backspace        : Go parent directory",
+            "Space            : Select, Unselect file",
+            "-/+              : Add or delete window",
         },
             true);
     }
